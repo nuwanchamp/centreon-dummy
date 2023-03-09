1a8a05b89dc1fad3eff256301aa3e692ee50bd87
nu-one <nuwan@synergysis.com>
2023-03-09 16:13:38 +0530

    revised installer

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 cp -R www/modules/dummy /usr/share/centreon/www/modules/dummy
 chown -R apache.  /usr/share/centreon/www/modules/dummy
 
-cp -R backend/config /usr/share/centreon/config
-cp -R backend/src /usr/share/centreon/src
+cp -R backend/config/packages/prod/CentreonDummy.yaml /usr/share/centreon/config/packages/prod
+cp -R backend/config/packages/serializer/*    /usr/share/centreon/config/packages/serializer
+cp -R backend/config/routes/*   /usr/share/centreon/config/routes/
+cp -R backend/src/* /usr/share/centreon/src/
 su - apache -s /bin/bash -c "/usr/share/centreon/bin/console cache:clear"
