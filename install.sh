536b853682dd688efec1f6304c1231b2377f58f0
nu-one <nuwan@synergysis.com>
2023-03-09 15:04:50 +0530

    Install sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cp -R www/modules/dummy /usr/share/centreon/www/dummy
+chown -R apache. /usr/share/centreon/www/dummy
+
+cp -R backend/config /usr/share/centreon/config
+cp -R backend/src /usr/share/centreon/src
+su - apache -s /bin/bash -c "/usr/share/centreon/bin/console cache:clear"
